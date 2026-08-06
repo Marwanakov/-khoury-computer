c5db90c01089676cb6a16defb2c966866ba15942
Marwan Khoury <marwan.william.khoury@gmail.com>
2026-08-06 03:44:44 +0200

    Add: admin dashboard foundation

---
+++ src/main/java/com/khourycomputer/web/controller/admin/AdminDashboardController.java
@@ -0,0 +1,13 @@
+package com.khourycomputer.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminDashboardController {
+
+    @GetMapping("/admin")
+    public String showDashboard() {
+        return "admin/dashboard";
+    }
+}
