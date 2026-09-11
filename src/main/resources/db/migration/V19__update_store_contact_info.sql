d40cec9fe7ebf2a2ba2299a260be8e926c54685c
Marwan Khoury <marwan.william.khoury@gmail.com>
2026-09-11 19:38:49 +0300

    Update: finalize production store contact information

---
+++ src/main/resources/db/migration/V19__update_store_contact_info.sql
@@ -0,0 +1,9 @@
+UPDATE store_contact_info
+SET
+    phone_number = '02-2744433',
+    facebook_messenger_link = 'https://m.me/khourycomputer',
+    whatsapp_number = '+972 598941102',
+    email = 'khouryco@palnet.com',
+    store_address_city = 'Bethlehem',
+    store_address_street = 'Manger St',
+    store_address_details = 'Opposite Bank of Jerusalem';
